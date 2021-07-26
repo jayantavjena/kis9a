8844dd3aecdb0a768e51d440f8dce44c689beb57
kis9a <kis9ax@gmail.com>
2021-07-26 20:12:47 +0900

    sources: update 2021-07-26 tools navigate

--- sources/src/pages/tools/index.js
+++ sources/src/pages/tools/index.js
@@ -1,53 +1,85 @@
 import { app, h, text } from "/modules/js/hyperapp.js";
-import { Hyperway } from "/modules/js/hyperway.js";
+import { Hyperway, routeTo } from "/modules/js/hyperway.js";
 import { Header } from "/components/header";
 import "/layouts/index.css";
+import "./index.css";
+
+const Link = ({ to }, t) =>
+  h("a", { class: `link`, onclick: routeTo(to) }, text(t));
 
 const defaultState = {
-  view: "home",
+  view: "default",
 };
 
 const initialState = [defaultState];
 
 const viewRoute = (view) => {
   switch (view) {
-    case "home":
-      return h("div", { class: "not-found" }, text("not found"));
-    case "about":
-      return h("div", { class: "not-found" }, text("not found"));
+    case "default":
+      return viewDefaults();
+    case "draw":
+      return viewDraw();
     default:
       return h("div", { class: "not-found" }, text("not found"));
   }
 };
 
+const viewDefaults = () => {
+  return h(
+    "div",
+    { style: { cursor: "pointer" } },
+    h("div", { class: "links" }, [
+      Link({ to: "/tools/" }, "TOOLS"),
+      Link({ to: "/tools/draw/" }, "DRAW"),
+    ])
+  );
+};
+
+const viewDraw = () => {
+  return h("h1", {}, text("draw"));
+};
+
+const back = (state) => {
+  return { ...state, view: "default" };
+};
+
 app({
   init: initialState,
   view: ({ view }) =>
-    h("div", { class: "container" }, [Header(), viewRoute(view)]),
+    h("div", { class: "container" }, [
+      view == "default"
+        ? Header()
+        : h(
+            "div",
+            { style: { margin: "2rem", cursor: "pointer" } },
+            h("h1", { onclick: back }, text("<"))
+          ),
+      h("main", { class: "main" }, [
+        h("div", { class: "content" }, viewRoute(view)),
+      ]),
+    ]),
   subscriptions: () => [
     Hyperway({
       onNotFound: (state, props) => {
-        console.log("NOT FOUND", props);
         return state;
       },
       onRoute: (state, props) => {
-        console.log("ROUTE", props);
         return { ...state, url: props.path, params: props.params };
       },
       routes: [
         {
-          path: "/",
+          path: "/tools/",
           onEnter: (state, _) => {
-            return { ...state, view: "home" };
+            return { ...state, view: "default" };
           },
           onLeave: (state, _) => {
             return state;
           },
         },
         {
-          path: "/#/about",
+          path: "tools/draw/",
           onEnter: (state, _) => {
-            return { ...state, view: "about" };
+            return { ...state, view: "draw" };
           },
         },
       ],
