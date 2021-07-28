2ece4a6e57f71a4a479ae05a3a3a173d866f7ddc
kis9a <kis9ax@gmail.com>
2021-07-28 10:43:12 +0900

    sources: update 2021-07-28 esbuild http plugin init

--- sources/cmd/bundle.go
+++ sources/cmd/bundle.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 
@@ -11,6 +13,63 @@ import (
 	"github.com/tdewolff/minify/v2"
 	"github.com/tdewolff/minify/v2/html"
 )
+
+var httpPlugin = api.Plugin{
+	Name: "http",
+	Setup: func(build api.PluginBuild) {
+		// Intercept import paths starting with "http:" and "https:" so
+		// esbuild doesn't attempt to map them to a file system location.
+		// Tag them with the "http-url" namespace to associate them with
+		// this plugin.
+		build.OnResolve(api.OnResolveOptions{Filter: `^https?://`},
+			func(args api.OnResolveArgs) (api.OnResolveResult, error) {
+				return api.OnResolveResult{
+					Path:      args.Path,
+					Namespace: "http-url",
+				}, nil
+			})
+
+		// We also want to intercept all import paths inside downloaded
+		// files and resolve them against the original URL. All of these
+		// files will be in the "http-url" namespace. Make sure to keep
+		// the newly resolved URL in the "http-url" namespace so imports
+		// inside it will also be resolved as URLs recursively.
+		build.OnResolve(api.OnResolveOptions{Filter: ".*", Namespace: "http-url"},
+			func(args api.OnResolveArgs) (api.OnResolveResult, error) {
+				base, err := url.Parse(args.Importer)
+				if err != nil {
+					return api.OnResolveResult{}, err
+				}
+				relative, err := url.Parse(args.Path)
+				if err != nil {
+					return api.OnResolveResult{}, err
+				}
+				return api.OnResolveResult{
+					Path:      base.ResolveReference(relative).String(),
+					Namespace: "http-url",
+				}, nil
+			})
+
+		// When a URL is loaded, we want to actually download the content
+		// from the internet. This has just enough logic to be able to
+		// handle the example import from unpkg.com but in reality this
+		// would probably need to be more complex.
+		build.OnLoad(api.OnLoadOptions{Filter: ".*", Namespace: "http-url"},
+			func(args api.OnLoadArgs) (api.OnLoadResult, error) {
+				res, err := http.Get(args.Path)
+				if err != nil {
+					return api.OnLoadResult{}, err
+				}
+				defer res.Body.Close()
+				bytes, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					return api.OnLoadResult{}, err
+				}
+				contents := string(bytes)
+				return api.OnLoadResult{Contents: &contents}, nil
+			})
+	},
+}
 
 func bundle() error {
 	if err := bundlePages(); err != nil {
@@ -183,6 +242,7 @@ func bundleJS(path string, wp string) error {
 		MinifyWhitespace:  !isDev,
 		Incremental:       true,
 		AllowOverwrite:    true,
+		Plugins:           []api.Plugin{httpPlugin},
 		Loader: map[string]api.Loader{
 			".png": api.LoaderDataURL,
 			".svg": api.LoaderText,
