9259f19384528936144a1c64d8cd3e35b26ffdd3
kis9a <kis9ax@gmail.com>
2021-08-04 20:45:10 +0900

    sources: update 2021-08-04

--- sources/src/pages/index.js
+++ sources/src/pages/index.js
@@ -1,16 +1,28 @@
-import { h, app } from "/modules/js/hyperapp.js";
+import { h, app, text } from "/modules/js/hyperapp.js";
 import { Header } from "/components/header";
 import "./index.css";
 import "/layouts/index.css";
 import { RSS } from "/modules/js/rss.js";
-const doper = require("/modules/js/doper.js");
+
+const defaultState = {
+  loading: false,
+};
+
+const initialState = [defaultState];
 
 app({
-  init: {},
-  view: () =>
+  init: initialState,
+  view: ({ loading }) =>
     h("div", { class: "container" }, [
       Header(),
-      h("main", { id: "rss-feeds" }, []),
+      h("main", { id: "rss-feeds" }, [
+        loading &&
+          h(
+            "h1",
+            { style: { width: "100%", textAlign: "left", padding: "20px" } },
+            text("Loading...")
+          ),
+      ]),
     ]),
   subscriptions: () => {},
   node: document.getElementById("app"),
@@ -28,88 +40,3 @@ window.onload = function () {
   );
   rss.render().then(() => {});
 };
-
-// (function () {
-//   const el = document.createElement("div");
-//   el.classList.add("box");
-//   document.body.appendChild(el);
-//   let node = doper("<div>RSS</div>", ".box");
-//   node.parent.style.position = "fixed";
-//   node.style.position = "absolute";
-//   node.style.fontSize = "60px";
-//   node.style.width = "50px";
-//   node.style.height = "50px";
-//   node.style.letterSpacing = "10px";
-//   node.minAnchorX = 0.2;
-//   node.maxAnchorX = 0.2;
-//   node.minAnchorY = 0.2;
-//   node.maxAnchorY = 0.2;
-//   function update() {
-//     node.rotation++;
-//     node.y = Math.sin(node.rotation * 0.05) * 100;
-//     requestAnimationFrame(update);
-//   }
-//   update();
-// })();
-
-// (function () {
-//   const el = document.createElement("div");
-//   el.classList.add("box1");
-//   document.body.appendChild(el);
-//   let node = doper("<div>Here</div>", ".box1");
-//   node.parent.style.position = "fixed";
-//   node.style.fontSize = "50px";
-//   node.style.width = "50px";
-//   node.style.height = "50px";
-//   node.style.letterSpacing = "8px";
-//   node.minAnchorX = 0.8;
-//   node.maxAnchorX = 0.8;
-//   node.minAnchorY = 0.2;
-//   node.maxAnchorY = 0.6;
-//   function update() {
-//     node.rotation++;
-//     node.y = Math.cos(node.rotation * 0.02) * 30;
-//     requestAnimationFrame(update);
-//   }
-//   update();
-// })();
-
-// function randomMove() {
-//   // var randomElm = document.getElementById(id);
-//   const randomElm = document.createElement("div");
-//   randomElm.parent.style.position = "fixed";
-//   randomElm.style.fontSize = "50px";
-//   document.body.appendChild(randomElm);
-//   var randomTop = 50;
-//   var randomLeft = 50;
-//   randomElm.style.top = randomTop + "%";
-//   randomElm.style.left = randomLeft + "%";
-//   setInterval(function () {
-//     if (Math.random() * 2 < 1) {
-//       randomTop++;
-//     } else {
-//       randomTop--;
-//     }
-//     if (randomTop === 0) {
-//       randomTop = 2;
-//     }
-//     if (randomTop === 100) {
-//       randomTop = 98;
-//     }
-//     if (Math.random() * 2 < 1) {
-//       randomLeft++;
-//     } else {
-//       randomLeft--;
-//     }
-//     if (randomLeft === 0) {
-//       randomLeft = 2;
-//     }
-//     if (randomLeft === 100) {
-//       randomLeft = 98;
-//     }
-//     randomElm.style.left = randomLeft + "%";
-//     randomElm.style.top = randomTop + "%";
-//   }, 100);
-// }
-
-// randomMove();
