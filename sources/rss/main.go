f034ae2b91a9a83ca38251fcd1aadc59f5d47a0c
kis9a <kis9ax@gmail.com>
2021-08-09 21:58:51 +0900

    action: fix rss.yml

--- sources/rss/main.go
+++ sources/rss/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 
-	"github.com/mmcdole/gofeed"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type MyEvent struct {
+	Name string `json:"What is your name?"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+
+func hello(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("Hello %s!!", event.Name)}, nil
+}
+
 func main() {
-	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://zenn.dev/kis9a/feed")
-	if err != nil {
-		log.Fatal(err)
-	}
-	items := feed.Items
-	res, err := json.Marshal(items)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(res))
+	lambda.Start(hello)
 }
