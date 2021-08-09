1a6f3b7711c975591126ae3584bfc8b1387f31e4
kis9a <kis9ax@gmail.com>
2021-08-09 11:40:57 +0900

    snippets: update 2021-08-09 add fk

---
+++ snippets/go/fk/go.mod
@@ -0,0 +1,8 @@
+module fk
+
+go 1.16
+
+require (
+	github.com/ktr0731/go-fuzzyfinder v0.4.0
+	github.com/mitchellh/go-ps v1.0.0
+)
---
+++ snippets/go/fk/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ktr0731/go-fuzzyfinder"
+	"github.com/mitchellh/go-ps"
+)
+
+var showVersion = flag.Bool("v", false, "show version")
+
+var version = "1.0.0"
+
+type process struct {
+	Pid int
+	Cmd string
+}
+
+func processes() ([]process, error) {
+	var processes []process
+	procs, err := ps.Processes()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, p := range procs {
+		// skip pid 0
+		if p.Pid() == 0 {
+			continue
+		}
+		processes = append(processes, process{
+			Pid: p.Pid(),
+			Cmd: p.Executable(),
+		})
+	}
+
+	return processes, nil
+}
+
+func kill(pid int) error {
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+
+	return p.Kill()
+}
+
+func main() {
+	flag.Parse()
+	if *showVersion {
+		fmt.Println("fk - " + version)
+		return
+	}
+
+	procs, err := processes()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	idx, err := fuzzyfinder.FindMulti(
+		procs,
+		func(i int) string {
+			return fmt.Sprintf("%d: %s", procs[i].Pid, procs[i].Cmd)
+		},
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	for _, i := range idx {
+		pid, cmd := procs[i].Pid, procs[i].Cmd
+		fmt.Println(pid, cmd)
+
+		if err := kill(pid); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
