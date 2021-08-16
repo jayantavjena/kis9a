469fe13e90c824c1046115919b161444d7ac215a
kis9a <kis9ax@gmail.com>
2021-08-16 13:18:11 +0900

    sources: update 2021-08-16 feat: terraform add ordered_cache_behavior

--- sources/terraform/modules/static_site/main.tf
+++ sources/terraform/modules/static_site/main.tf
@@ -62,12 +62,33 @@ resource "aws_cloudfront_distribution" "static_site" {
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 86400
+    default_ttl            = 172800
+    max_ttl                = 2592000
+    compress               = true
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
       cookies {
         forward = "none"
       }
