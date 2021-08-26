3f75a28187b1a62c22df19086a212865e4472319
kis9a <kis9ax@gmail.com>
2021-08-26 14:09:30 +0900

    sources: update 2021-08-26 feat:ux: display to top button when scroll up any px

--- sources/src/components/top/index.js
+++ sources/src/components/top/index.js
@@ -3,15 +3,32 @@ import { fadeIn, fadeOut } from "/modules/js/fadeInOut.js";
 import svg_top from "/assets/svgs/chevron-double-up.svg";
 import "./index.css";
 
+const delimiter = 15;
+let amount = document.body.scrollTop;
+
+const isUpScroll = (() => {
+  const el = "sel" in document ? document.sel : document.documentElement;
+  let sp, preSp, is;
+  return () => {
+    sp = el.scrollTop;
+    is = preSp > sp ? true : false;
+    preSp = sp;
+    return is;
+  };
+})();
+
 onscroll = () => {
+  amount = isUpScroll() ? ++amount : 0;
   const top = document.getElementById("top");
-  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  if (scrollTop > 600) {
+  const scrollTop =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  console.log(amount);
+  if (scrollTop > 600 && amount > delimiter) {
     if (top.classList.contains("hide")) {
-      fadeIn(top, 500);
+      fadeIn(top, 400);
     }
     top.classList.remove("hide");
-  } else if (scrollTop < 500) {
+  } else {
     if (!top.classList.contains("hide")) {
       fadeOut(top, 200);
       setTimeout(() => {
@@ -31,8 +48,6 @@ export const Top = () => {
 };
 
 const toTop = (state) => {
-  // document.body.scrollTop = 0;
-  // document.documentElement.scrollTop = 0;
   window.scrollTo({
     top: 0,
     left: 0,
